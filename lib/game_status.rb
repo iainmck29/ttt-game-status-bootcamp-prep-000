478baf18ad223e811325d170e203e9cad7365030
Iain McKenzie <iainmck29@hotmail.com>
2018-09-27 10:19:06 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,59 +4,66 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-
 WIN_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
-  [0, 4, 8],
-  [2, 4, 6],
   [0, 3, 6],
   [1, 4, 7],
-  [2, 5, 8]
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
 ]
 
-def position_taken?(board, index)
- !(board[index] == " " || board[index] == "" || board[index] == nil)
-end
-
 def won?(board)
-  empty_board = board.all? {|x| x == " "}
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
 
-  WIN_COMBINATIONS.each do |winners|
-      win_index_1 = winners[0]
-      win_index_2 = winners[1]
-      win_index_3 = winners[2]
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
 
-      position_1 = board[win_index_1]
-      position_2 = board[win_index_2]
-      position_3 = board[win_index_3]
-
-      if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
-        return winners
-        return true
-      elsif empty_board == true
-    return false
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
     end
-  end
+  }
   return false
 end
 
-
 def full?(board)
-  board.all? {|taken| taken == "X" || taken == "O"}
+  board.all? {|index| index == "X" || index == "O"}
 end
 
 def draw?(board)
-  if full?(board) == true && won?(board) == false
-    return true
-  end
-end
-
-def over?(new_board)
-  if won?(new_board) == true || draw?(new_board) == true || full?(new_board) == true
+  if !won?(board) && full?(board)
     return true
   else
     return false
+  end
+end
+
+def over?(board)
+  if won?(board) || full?(board) || draw?(board)
+    return true
+  else
+    return false
+  end
+end
+
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  else
+    if board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
   end
 end
